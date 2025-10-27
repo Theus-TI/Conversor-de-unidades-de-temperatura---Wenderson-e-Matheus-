305ff55390de64c3766a464a6f0057b199ade410
Theus-TI <matheushenriquesouza001@gmail.com>
2025-10-27 18:33:48 -0300

    chore(init): scaffold do projeto estático

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const $ = (sel) => document.querySelector(sel);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const fromValue = $('#fromValue');
+  const toValue = $('#toValue');
+  const fromUnit = $('#fromUnit');
+  const toUnit = $('#toUnit');
+});
